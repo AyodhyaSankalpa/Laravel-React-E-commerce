842e75a752ff7450c52061a6591e59a2a6d69696
Ayodhya Sankalpa <ayodhyasankalpa2@gmail.com>
2025-10-11 06:35:11 +0530

    Brand API
    Create brand API for brand crud

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\admin\AuthController;
+use App\Http\Controllers\admin\BrandController;
 use App\Http\Controllers\admin\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,5 +20,5 @@ Route::group(['middleware' => 'auth:sanctum'],function(){
     // Route::post('categories',[CategoryController::class,'store']);
 
     Route::resource('categories',CategoryController::class);
-    Route::resource('brands',CategoryController::class);
+    Route::resource('brands',BrandController::class);
 });
