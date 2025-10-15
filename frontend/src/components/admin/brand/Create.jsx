623af075df312bd863926fe46e6ccc807e192891
Ayodhya Sankalpa <ayodhyasankalpa2@gmail.com>
2025-10-15 21:31:34 +0530

    Create CRUD for Brand

--- frontend/src/components/admin/brand/Create.jsx
+++ frontend/src/components/admin/brand/Create.jsx
@@ -3,10 +3,12 @@ import Layout from '../../common/Layout'
 import Sidebar from '../../common/Sidebar'
 import { Link, useNavigate } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
-import { toast } from 'react-toastify'
 import { adminToken, apiUrl } from '../../common/http'
+import { toast } from 'react-toastify'
+
 
 const Create = () => {
+
     const [disable, setDisable] = useState(false)
     const navigate = useNavigate();
 
@@ -17,10 +19,9 @@ const Create = () => {
         formState: { errors },
     } = useForm();
 
-    const saveCategory = async (data) => {
-      setDisable(true);
-        console.log(data)
-        const res = await fetch(`${apiUrl}/categories`,{
+    const saveBrand = async (data) => {
+        setDisable(true);
+        const res = await fetch(`${apiUrl}/brands`,{
             method: 'POST',
             headers: {
                 'Content-type' : 'application/json',
@@ -28,72 +29,75 @@ const Create = () => {
                 'Authorization' : `Bearer ${adminToken()}`
             },
             body: JSON.stringify(data)
-        }).then(res => res.json())
+        })
+        .then(res => res.json())
         .then(result => {
             setDisable(false);
             if (result.status == 200) {
                 toast.success(result.message);
-                navigate('/admin/categories')
+                navigate('/admin/brands')
             } else {
                 console.log("Something went wrong")
             }
             
         })
     }
-
+    
   return (
     <Layout>        
         <div className="container">
           <div className="row">
             <div className="d-flex justify-content-between mt-5 pb-3">
-              <h3 className="h4 pb-0 mb-0">Categories / Create</h3>
-              <Link to="/admin/categories" className="btn btn-primary">Back</Link>
+              <h3 className="h4 pb-0 mb-0">Brands / Create</h3>
+              <Link to="/admin/brands" className="btn btn-primary">Back</Link>
             </div>
             <div className="col-md-3">
               <Sidebar/>
             </div>
             <div className="col-md-9">
-                <form onSubmit={handleSubmit(saveCategory)}>
+
+                <form onSubmit={handleSubmit(saveBrand)}>
                     <div className="card shadow">
                         <div className="card-body p-4">
                             <div className='mb-3'>
                                 <label htmlFor="" className='form-label'>Name</label>
                                 <input 
-                                  {
+                                {
                                     ...register('name',{
-                                      required : 'The name field is required'
+                                    required : 'The name field is required'
                                     })
-                                  }
-                                  type="text" 
-                                  className={`form-control ${errors.name && 'is-invalid'}`}  
-                                  placeholder='Name'/>
-                                  {
-                                     errors.name && <p className='invalid-feedback'>{errors.name?.message}</p>
-                                  }
+                                }
+                                type="text" 
+                                className={`form-control ${errors.name && 'is-invalid'}`}  
+                                placeholder='Name'/>
+                                {
+                                    errors.name && <p className='invalid-feedback'>{errors.name?.message}</p>
+                                }
                             </div>
                             <div className='mb-3'>
                                 <label htmlFor="" className='form-label'>Status</label>                                
                                 <select 
-                                  {
+                                {
                                     ...register('status',{
-                                      required : 'Please select a status'
+                                    required : 'Please select a status'
                                     })
-                                  }
+                                }
                                 className={`form-control ${errors.status && 'is-invalid'}`}  >
                                     <option value="">Select a Status</option>
                                     <option value="1">Active</option>
                                     <option value="0">Block</option>
                                 </select>
-                                  {
-                                     errors.status && <p className='invalid-feedback'>{errors.status?.message}</p>
-                                  }
+                                {
+                                    errors.status && <p className='invalid-feedback'>{errors.status?.message}</p>
+                                }
                             </div>
                         </div>
                     </div>
                     <button 
                     disabled={disable}
                     type='submit' className="btn btn-primary mt-3">Create</button>
-                </form>                
+                </form>
+                                    
             </div>
           </div>
         </div>
