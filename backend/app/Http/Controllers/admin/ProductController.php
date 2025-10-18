d74ba6d945392c8fa7b8f27aba62976367b56078
Ayodhya Sankalpa <ayodhyasankalpa2@gmail.com>
2025-10-18 18:30:41 +0530

    Product API
    Create product update API

--- backend/app/Http/Controllers/admin/ProductController.php
+++ backend/app/Http/Controllers/admin/ProductController.php
@@ -69,9 +69,56 @@ class ProductController extends Controller
 
     }
 
-    public function update()
+    public function update($id, Request $request)
     {
+        $product = Product::find($id);
 
+        if ($product == null ) {
+
+            return response()->json([
+            'status' => 404,
+            'message' => 'Product not found'
+            ],404 );
+
+        }
+
+        $validator = Validator::make($request->all(),[
+            'title' => 'required',
+            'price' => 'required|numeric',
+            'category' => 'required|integer',
+            'sku' => 'required|unique:products,sku,'.$id.',id',
+            'is_featured' => 'required',
+            'status' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 400,
+                'errors' => $validator->errors()
+            ],400);
+        }
+
+        //update the product
+
+        $product->title = $request->title;
+        $product->price = $request->price;
+        $product->compare_price = $request->compare_price;
+        $product->category_id = $request->category;
+        $product->brand_id = $request->brand;
+        $product->sku = $request->sku;
+        $product->qty = $request->qty;
+        $product->description = $request->description;
+        $product->short_description = $request->short_description;
+        $product->status = $request->status;
+        $product->is_featured = $request->is_featured;
+        $product->barcode = $request->barcode;
+        $product->save();
+
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Product has been updated successfully'
+        ],200);
     }
 
     public function destroy()
