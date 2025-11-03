da84537e0b1aa5826432bb0cd1193e9af2178ebe
Ayodhya Sankalpa <ayodhyasankalpa2@gmail.com>
2025-11-03 22:35:15 +0530

    Shop Page
    Show product , Category & Brand Filter

--- backend/app/Http/Controllers/front/ProductController.php
+++ backend/app/Http/Controllers/front/ProductController.php
@@ -3,11 +3,41 @@
 namespace App\Http\Controllers\front;
 
 use App\Http\Controllers\Controller;
+use App\Models\Brand;
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+
+    public function getProducts(Request $request)
+    {
+        $products = Product::orderBy('created_at','DESC')
+                    ->where('status',1);
+
+        // Filter product by category
+        if (!empty($request->category))
+        {
+            $catArray = explode(',',$request->category);
+            $products = $products->whereIn('category_id', $catArray);
+        }
+
+        // Filter product by brand
+        if (!empty($request->brand))
+        {
+            $brandArray = explode(',',$request->brand);
+            $products = $products->whereIn('brand_id', $brandArray);
+        }
+
+        $products = $products->get();
+
+        return response()->json([
+            'status' => 200,
+            'data' => $products
+        ],200);
+    }
+
     public function latestProducts()
     {
         $products = Product::orderBy('created_at','DESC')
@@ -31,5 +61,27 @@ class ProductController extends Controller
             'satus' => 200,
             'data' => $products
         ],200);
+    }
+
+    public function getCateories()
+    {
+        $categories = Category::orderBy('name','ASC')
+            ->where('status',1)
+            ->get();
+            return response()->json([
+                'status' => 200,
+                'data' => $categories
+            ],200);
+    }
+
+    public function getBrands()
+    {
+        $brands = Brand::orderBy('name','ASC')
+            ->where('status',1)
+            ->get();
+            return response()->json([
+                'status' => 200,
+                'data' => $brands
+            ],200);
     }
 }
