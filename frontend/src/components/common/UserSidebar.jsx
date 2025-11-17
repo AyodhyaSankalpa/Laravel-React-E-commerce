8399ef94a6ce28dc83db6927d71b3ea113db2dcd
Ayodhya Sankalpa <ayodhyasankalpa2@gmail.com>
2025-11-17 19:05:14 +0530

    Create My Order
    Create my order page for users

--- frontend/src/components/common/UserSidebar.jsx
+++ frontend/src/components/common/UserSidebar.jsx
@@ -14,7 +14,7 @@ const {logout} = useContext(AuthContext);
                       <Link to="/account">Account</Link>
                     </li>
                     <li>
-                      <Link to="#">Orders</Link>
+                      <Link to="/account/orders">Orders</Link>
                     </li>
                     <li>
                       <Link to="#">Change Password</Link>
